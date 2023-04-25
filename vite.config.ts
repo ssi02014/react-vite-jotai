87334c589b7f6069241c2616cd1f35c6570a2d60
ssi02014 <ssi02014@naver.com>
2023-04-25 17:44:06 +0900

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,25 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3000,
+  },
   build: {
     outDir: "build",
+    rollupOptions: {
+      output: {
+        assetFileNames: (asset) => {
+          const isCssFile = /\.css$/.test(asset.name);
+
+          if (isCssFile) {
+            return "static/css/[name].[hash][ext]";
+          }
+          return "static/media/[name].[hash][ext]";
+        },
+        chunkFileNames: "static/js/[name].[hash].chunk.js",
+        entryFileNames: "static/js/[name].[hash].js",
+      },
+    },
   },
-  plugins: [react(), tsconfigPaths()],
 });
