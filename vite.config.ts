0ae274b78a7e365bc388178705e219cc017a32ae
ssi02014 <ssi02014@naver.com>
2023-05-01 23:43:03 +0900

    feat: svgr() 패키지 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   server: {
     port: 3000,
   },
@@ -16,13 +17,19 @@ export default defineConfig({
           const isCssFile = /\.css$/.test(asset.name);
 
           if (isCssFile) {
-            return "static/css/[name].[hash][ext]";
+            return "static/css/[name].[hash].[ext]";
           }
-          return "static/media/[name].[hash][ext]";
+          return "static/media/[name].[hash].[ext]";
         },
         chunkFileNames: "static/js/[name].[hash].chunk.js",
         entryFileNames: "static/js/[name].[hash].js",
       },
     },
+    commonjsOptions: {
+      include: ["node_modules/**"],
+    },
+  },
+  define: {
+    global: "globalThis",
   },
 });
